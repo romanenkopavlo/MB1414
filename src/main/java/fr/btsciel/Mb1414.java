65963d0cbf9ce3d3594dab18ab014afec5db4259
pavlo <paihitetty444@gmail.com>
2024-02-08 09:03:37 +0100

    La version definitive

--- src/main/java/fr/btsciel/Mb1414.java
+++ src/main/java/fr/btsciel/Mb1414.java
@@ -3,35 +3,58 @@ package fr.btsciel;
 import jssc.SerialPortEvent;
 import jssc.SerialPortException;
 
-import java.util.ArrayList;
+import java.text.DecimalFormat;
+import java.time.Duration;
+import java.time.Instant;
 
 public class Mb1414 extends LiaisonSerie {
-    private double distance;
-
+    double distance = 0.0;
+    double distanceInit = 0.0, vitesseInit = 0.0;
+    double distanceFinal, distanceDelta, vitesseFinal, vitesseDelta, acceleration;
+    DecimalFormat dfDistance = new DecimalFormat("0.## cm");
+    DecimalFormat dfDeltaT=new DecimalFormat("0.## s");
+    DecimalFormat dfVitesseAcceleration = new DecimalFormat("0.##");
+    Instant tInit, tFinal;
     public Mb1414() {
+        tInit=Instant.now();
     }
-    public void calcul (SerialPortEvent spe) {
+    @Override
+    public void serialEvent(SerialPortEvent event) {
+        tFinal=Instant.now();
+        calcul(event);
+        if (distanceDelta != 0) {
+            System.out.printf("""
+                %s
+                __________________________
+                Delta d = %s
+                Delta t = %s
+                Vitesse = %s cm/s
+                Acceleration = %s cm/s²
+                %n""", dfDistance.format(distance), dfDistance.format(distanceDelta), dfDeltaT.format((Duration.between(tInit,tFinal).toMillis())*0.001), dfVitesseAcceleration.format(vitesseFinal), dfVitesseAcceleration.format(acceleration));
+        }
+        tInit=tFinal;
+    }
 
+    private void calcul(SerialPortEvent event) {
+        byte[] laTrame = lireTrame(event.getEventValue());
+        if(laTrame.length==8){
+            Instant.now();
+            distance=((laTrame[1]-0x30)*100 + (laTrame[2]-0x30)*10 + (laTrame[3]-0x30))*2.54;
+        }
+        distanceFinal = distance;
+        distanceDelta = distanceFinal - distanceInit;
+        distanceInit = distanceFinal;
+
+        vitesseFinal = (((distanceDelta))/((Duration.between(tInit, tFinal).toMillis())*0.001));
+        vitesseDelta = vitesseFinal - vitesseInit;
+        vitesseInit = vitesseFinal;
+
+        acceleration = (((vitesseDelta))/((Duration.between(tInit, tFinal).toMillis()) * 0.001));
     }
-    public void deconnexionCapteur() {
-        super.fermerPort();
-    }
-    public void initialisationCapteur(String port) throws SerialPortException {
-        initCom(port);
+
+    public void initialisationCapteur(String lePortCom) throws SerialPortException {
+        super.initCom(lePortCom);
         super.configurerParametres(57600, 8, 0, 1);
-    }
-    public double getDistance() {
-        return distance;
-    }
-    public ArrayList<String> listerLesPorts() {
-        return super.listerLesPorts();
-    }
-    public void serialEvent(SerialPortEvent spe) {
-        byte [] laTrame;
-        int longeur;
-        longeur = spe.getEventValue();
-        laTrame = lireTrame(longeur);
-        distance = laTrame[0] & 0xff;
-        System.out.println(distance);
+
     }
 }
